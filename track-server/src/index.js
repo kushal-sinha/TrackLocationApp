6c103783187421772e590cbe6214001780b43822
kushal-sinha <kushal147258369@gmail.com>
2025-01-22 13:06:24 +0530

    first commit

---
+++ track-server/src/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+const mongoUri = "mongodb+srv://kushal999taken:S8cfzLSyxW8U9oGw@cluster0.vgcld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
